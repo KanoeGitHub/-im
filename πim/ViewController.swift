4fb8639c765147fc73651f003ccae922047908fc
Kanoe <estancia.k_0493@outlook.jp>
2018-06-09 17:25:34 +0900

    色々

--- πim/ViewController.swift
+++ πim/ViewController.swift
@@ -23,12 +23,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    let pi01 = [1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3,2,7,9,5,0,2,8,8,4,1,9,7,1,6,9,3,9,9,3,7,5,1,0,5,8,2,0,9,7,4,9,4,4,5,9,2,3,0,7,8,1,6,4,0,6,2,8,6,2,0,8,9,9,8,6,2,8,0,3,4,8,2,5,3,4,2,1,1,7,0,6,7,9,8,2,1,4,8,0,8,6,5,1,3,2,8,2,3,0,6,6,4,7,0,9,3,8,4,4,6,0,9,5,5,0,5,8,2,2,3,1,7,2,5,3,5,9,4,0,8,1,2,8]
+    let pi01 = makePi().pi   
     var counter = 0
     var answerString = "3."
     
     @IBOutlet weak var piView: UILabel!
+    
     
     @IBAction func zeroButton(_ sender: Any) {
         if pi01[counter] == 0{
@@ -44,6 +44,7 @@ class ViewController: UIViewController {
             piView.text = "Finish"
         }
     }
+    
     @IBAction func oneButton(_ sender: Any) {
         if pi01[counter] == 1{
         answerString = answerString + "1"
@@ -171,7 +172,6 @@ class ViewController: UIViewController {
             piView.text = "Finish"
         }
     }
-    
     
     @IBAction func resetButton(_ sender: Any) {
         answerString = "3."
